a3d144599c7d1aeb9543ff652f7e7c307aee5f97
Sebastian Ramirez <sebastianramirez2193@gmail.com>
2026-06-03 20:45:59 -0500

    fix: agregar interceptor para token

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,18 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "http://localhost:3001"
+const api = axios.create({
+  baseURL: "https://sistemaparqueadero-lr67.onrender.com"
 });
+
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
