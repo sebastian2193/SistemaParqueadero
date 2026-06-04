bd25653fce08b60ca3612d7cb074a95c032e9420
Sebastian Ramirez <sebastianramirez2193@gmail.com>
2026-06-03 22:22:24 -0500

    fix: mysql pool connection for render

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,19 +1,14 @@
 const mysql = require("mysql2");
-require("dotenv").config();
 
-const conexion = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-conexion.connect((error) => {
-  if (error) {
-    console.log("Error de conexión:", error);
-  } else {
-    console.log("Base de datos conectada");
-  }
+const conexion = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = conexion;
